e82e1c2d6f2355de18fb12f749257581e73355f4
jayedbin12 <57245337+jayedbin12@users.noreply.github.com>
2021-09-26 09:25:24 +0600

    Update form.php

--- Task1/form.php
+++ Task1/form.php
@@ -8,33 +8,42 @@
             <hr>
             <table>
                 <tr>
-                    <td>First name <input type="text" name="fname" pattern="[A-Za-z]{5}"></td>
+                    <td>First name</td>
+                    <td><input type="text" name="fname" pattern="[A-Za-z]{5}"></td>
                 </tr>
                 <tr>
-                    <td>Last name <input type="text" name="lname" pattern="[A-Za-z]{5}"></td>
+                    <td>Last name</td>
+                    <td><input type="text" name="lname" pattern="[A-Za-z]{5}"></td>
                 </tr>
                 <tr>
-                    <td>Age <input type="text" name="age"></td>
+                    <td>Age</td>
+                    <td> <input type="text" name="age"></td>
                 </tr>
                 <tr>
-                    <td>Designation 
+                    <td>Designation
+                        <td>
                         <input type="radio" name="designation" value="Junior Programmer">Junior Programmer
                         <input type="radio" name="designation" value="Senior Programmer">Senior Programmer
                         <input type="radio" name="designation" value="Team leader">Team leader
+                        </td>    
                     </td>
                 </tr>
                 <tr>
                     <td>Preferred language
+                        <td>
                         <input type="checkbox" name="language" value="JAVA">JAVA
                         <input type="checkbox" name="language" value="PHP">PHP
                         <input type="checkbox" name="language" value="C++">C++
+                        </td>   
                     </td>
                 </tr>
                 <tr>
-                    <td>Email <input type="email" id="email" name="email"></td>
+                    <td>Email</td>
+                    <td> <input type="email" id="email" name="email"></td>
                 </tr>
                 <tr>
-                    <td>Password <input type="password" id="password" name="password" pattern=".{8,}"></td>
+                    <td>Password</td>
+                    <td><input type="password" id="password" name="password" pattern=".{8,}"></td>
                 </tr>
             </table>
             <input type="submit" name="submit" value="Submit">
