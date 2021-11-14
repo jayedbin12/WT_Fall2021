9acd16f69566060839436e2b9d6a415a4bd6b8eb
jayedbin12 <57245337+jayedbin12@users.noreply.github.com>
2021-11-14 18:04:50 +0600

    Add files via upload

---
+++ fttask1/task.php
@@ -0,0 +1,62 @@
+<html>
+    <head>
+        <title>task1</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="texts">
+                <h1>ABC Managment System</h1>
+                <h4>We Create Future</h4>
+            </div>
+            <div class="dashboard">
+                <p>Home</p>
+                <p>About US</p>
+                <p>Shop</p>
+                </div>
+            <div class="title"><h2>Registration Form</h2></div>
+            <hr>
+            <div class="form">
+            <form action="" method="post">
+                <table>
+                    <tr>
+                        <td>First Name:</td>
+                        <td><input type="text" name="fname"></td>
+                    </tr>
+                    <tr>
+                        <td>Last Name:</td>
+                        <td><input type="text" name="lname"></td>
+                    </tr>
+                    <tr>
+                        <td>Age:</td>
+                        <td><input type="text" name="age"></td>
+                    </tr>
+                    <tr>
+                        <td>Designation:</td>
+                        <td><input type="radio">Junior Programmer
+                    <input type="radio">Senior Programmer
+                    <input type="radio">Project Leader
+                    </td>
+                    </tr>
+                    <tr>
+                        <td>Email:</td>
+                        <td><input type="email"></td>
+                    </tr>
+                    <tr>
+                        <td>Password:</td>
+                        <td><input type="password"></td>
+                    </tr>
+                    <tr>
+                        <td>Please Choose a file:</td>
+                        <td><input type="file"></td>
+                    </tr>
+                </table>
+                <div class="button">
+                    <input type="submit" value="Submit">
+                    <input type="reset" value="Reset">
+                </div>
+            </form>
+            </div>
+        </div>
+    </body>
+</html>
